64977df6462ca48468d83f85d8902a63f4b90265
Julius Tens <mail@juliustens.eu>
2019-06-13 07:42:10 +0200

    add endpoints

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const express = require('express')
 const http = require('http')
 const corser = require('corser')
 const compression = require('compression')
+const bodyParser = require('body-parser')
+const path = require('path')
+const fs = require('fs')
+const pify = require('pify')
+const get = require('lodash/get')
+const pick = require('lodash/pick')
+
+const port = process.env.PORT || '3000'
+const volumeDirectory = process.env.VOLUME_DIRECTORY || '/mnt/europa_volume'
+const dataPath = path.resolve(volumeDirectory, './data.ndjson')
+
+const promisedAppendFile = pify(fs.appendFile)
+const appendData = line => promisedAppendFile(dataPath, line)
 
 const api = express()
 const server = http.createServer(api)
@@ -11,6 +24,53 @@ const server = http.createServer(api)
 const allowed = corser.simpleRequestHeaders.concat(['User-Agent'])
 api.use(corser.create({ requestHeaders: allowed })) // CORS
 api.use(compression())
+api.use(bodyParser.json({ type: '*/*' }))
+
+const validateRecording = recording => {
+	const bssid = get(recording, 'wifi.bssid')
+	if (typeof bssid !== 'string' || bssid.length < 1) return null
+
+	const ssid = get(recording, 'wifi.ssid')
+	if (typeof ssid !== 'string' || ssid.length < 1) return null
+
+	const station = get(recording, 'station')
+	if (typeof station !== 'string' || station.length !== 12) return null
+
+	const direction = get(recording, 'direction')
+	if (typeof direction !== 'string' || direction.length !== 12) return null
+
+	const line = get(recording, 'line')
+	if (typeof line !== 'string' || line.length < 1) return null
+
+	return {
+		wifi: pick(recording.wifi, ['ssid', 'bssid']),
+		...pick(recording, ['line', 'station', 'direction'])
+	}
+}
+
+api.get('/data.ndjson', (req, res, next) => {
+	res.setHeader('content-type', 'application/x-ndjson')
+	const readStream = fs.createReadStream(dataPath)
+	readStream.on('error', error => {
+		error.code = 500
+		next(error)
+	})
+	readStream.pipe(res)
+})
+
+api.post('/', async (req, res, next) => {
+	const recording = get(req, 'body')
+	const validatedRecording = validateRecording(recording)
+	if (!validatedRecording) return next({ statusCode: 400, message: 'Invalid recording' })
+
+	try {
+		await appendData(JSON.stringify(validatedRecording) + '\n')
+		res.status(200).json({ ok: true })
+	} catch (error) {
+		error.code = 500
+		next(error)
+	}
+})
 
 api.use((err, req, res, next) => {
 	if (res.headersSent) return next(err)
--- readme.md
+++ readme.md
@@ -2,10 +2,12 @@
 
 Backend for the [BVG WiFi recording app](https://github.com/juliuste/bvg-wifi-recording-app).
 
-[![dependency status](https://img.shields.io/david/juliuste/bvb-wifi-recording-rest.svg)](https://david-dm.org/juliuste/bvb-wifi-recording-rest)
-[![license](https://img.shields.io/github/license/juliuste/bvb-wifi-recording-rest.svg?style=flat)](LICENSE)
+**You can obtain the most recent dataset of WiFi BSSIDs [here](https://bvg-wifi-recording.juliustens.eu/data.ndjson).**
+
+[![dependency status](https://img.shields.io/david/juliuste/bvg-wifi-recording-rest.svg)](https://david-dm.org/juliuste/bvg-wifi-recording-rest)
+[![license](https://img.shields.io/github/license/juliuste/bvg-wifi-recording-rest.svg?style=flat)](LICENSE)
 [![chat on gitter](https://badges.gitter.im/juliuste.svg)](https://gitter.im/juliuste)
 
 ## Contributing
 
-If you found a bug, want to propose a feature or feel the urge to complain about your life, feel free to visit [the issues page](https://github.com/juliuste/bvb-wifi-recording-rest/issues).
+If you found a bug, want to propose a feature or feel the urge to complain about your life, feel free to visit [the issues page](https://github.com/juliuste/bvg-wifi-recording-rest/issues).
